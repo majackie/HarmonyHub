af555ef4ac9bb62ec608532734700d0d72993765
Jackie Ma <97272592+majackie@users.noreply.github.com>
2023-11-09 19:33:27 -0800

    Initial Commit

---
+++ HarmonyHub/HarmonyHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyHubApp.swift
+//  HarmonyHub
+//
+//  Created by Jackie Ma on 2023-11-09.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
